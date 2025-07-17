287e732b8d428d096cc42153b85f4ae0f8d0e107
yuanlehome <yuanlehome@163.com>
2025-07-17 14:46:26 +0800

    update compare

--- fastdeploy/rl/test_rollout_model.py
+++ fastdeploy/rl/test_rollout_model.py
@@ -1,9 +1,12 @@
+import difflib
+
 from paddleformers.trl.llm_utils import init_dist_env
 
 from fastdeploy.rl.rollout_config import RolloutModelConfig
 from fastdeploy.rl.rollout_model import RolloutModel
 
 _, ranks = init_dist_env()
+
 
 # MODEL_PATH = "/root/paddlejob/workspace/env_run/output/EB45T-21B-Paddle"
 MODEL_PATH = "/root/paddlejob/workspace/env_run/output/ernie-4_5-vl-28b-a3b-bf16-paddle/"
@@ -31,8 +34,21 @@ for k, v in actor_eval_model.get_name_mappings_to_training().items():
 # with open("baseline.txt", "w", encoding="utf-8") as f:
 #     f.write(baseline)
 
+def compare_strings(a: str, b: str) -> bool:
+    if a == b:
+        print("✅ 两个字符串完全一致")
+        return True
+
+    print("❌ 字符串不一致，差异如下（上下文差异显示）：")
+    diff = difflib.ndiff(a.splitlines(), b.splitlines())
+    for line in diff:
+        if line.startswith("- ") or line.startswith("+ "):
+            print(line)
+
+    return False
+
 with open("baseline.txt", "r", encoding="utf-8") as f:
     baseline = f.read()
-    assert content != baseline, "In the unittest of RL scenario, your modification " \
+    assert compare_strings(baseline, content), "In the unittest of RL scenario, your modification " \
         "caused inconsistency in the content before and after. Please fix it. " \
         "Can request assistance from yuanlehome or gzy19990617 (github id)."
